59edff9539ab0247c2797407cec157414d307f3e
jade roland eduard <jaderolandeduard.pinon@lorma.edu>
2026-09-18 21:46:15 +0800

    reflection md and sql commands uploaded

---
+++ week5/commands.sql
@@ -0,0 +1,7 @@
+SELECT * FROM Pet;
+SELECT name,species FROM Pet;
+SELECT name,species FROM Pet WHERE species = 'Dog';
+SELECT name,species FROM Pet WHERE age > 3;
+SELECT appointment_date FROM Appointment WHERE appointment_date >= '2026-09-20';
+SELECT name FROM Pet WHERE species = 'Cat';
+SELECT name FROM Pet WHERE species = 'Cat;   
